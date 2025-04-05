0dccad3e6955dc1b1f1815ba3735b6a8681bff82
Bakadax <bakadax@qq.com>
2025-04-05 12:31:53 +0800

    修

--- src/plugins/chat/bot.py
+++ src/plugins/chat/bot.py
@@ -26,7 +26,7 @@ from .chat_stream import chat_manager
 from .message_sender import message_manager  # 导入新的消息管理器
 from .relationship_manager import relationship_manager
 from .storage import MessageStorage
-from .utils import is_mentioned_bot_in_message, get_recent_group_detailed_plain_text, is_reply_bot_in_message
+from .utils import is_mentioned_bot_in_message, get_recent_group_detailed_plain_text
 from .utils_image import image_path_to_base64
 from .utils_user import get_user_nickname, get_user_cardname
 from ..willing.willing_manager import willing_manager  # 导入意愿管理器
