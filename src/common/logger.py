ac27cfec97d65eda413c9c04d86555b646236ea2
Bakadax <bakadax@qq.com>
2025-05-17 07:19:03 +0800

    fix

--- src/common/logger.py
+++ src/common/logger.py
@@ -6,7 +6,7 @@ from types import ModuleType
 from pathlib import Path
 from dotenv import load_dotenv
 
-from src.config.config import global_config
+# from src.config.config import global_config
 
 
 # 加载 .env 文件
@@ -14,7 +14,7 @@ env_path = Path(os.getcwd()) / ".env"
 load_dotenv(dotenv_path=env_path)
 
 # 从全局配置中获取机器人昵称
-bot_nickname = global_config.bot.nickname
+bot_nickname = "枫の"
 
 # 保存原生处理器ID
 default_handler_id = None
